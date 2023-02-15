e238962270de0ec67e6d04e43d93ffa725c4e304
Ali Ibrahim <52763138+Ali-Ibrahim522@users.noreply.github.com>
2023-02-15 20:58:41 +0000

    starting

---
+++ memory.c
@@ -0,0 +1,30 @@
+//imports
+
+//globals
+#define MEMSIZE 80
+char *mem[MEMSIZE];
+
+void allocateMem() {
+
+}
+
+void freeMem() {
+
+}
+
+void compactMem() {
+
+}
+
+void showMem() {
+
+}
+
+void processFile() {
+
+}
+
+int main(int argc, char **argv) {
+  memset(mem, '.', sizeof(char) * MEMSIZE);
+  
+}
