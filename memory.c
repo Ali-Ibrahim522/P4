ee2b4933913436d9b5bf4fb0466349a5fb7823dc
Ali-Ibrahim522 <52763138+Ali-Ibrahim522@users.noreply.github.com>
2023-02-24 00:55:58 +0000

    freelo ass no kizzy

--- memory.c
+++ memory.c
@@ -1,14 +1,25 @@
 //imports
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 //globals
 #define MEMSIZE 80
-char *mem[MEMSIZE];
+struct node {
+  char name;
+  int size;
+  struct node *next;
+};
+typedef struct node node;
 
-void allocateMem() {
+node* head;
+
+void allocateMem(char name, int size, char algo) {
 
 }
 
-void freeMem() {
+void freeMem(char name) {
 
 }
 
@@ -20,11 +31,38 @@ void showMem() {
 
 }
 
-void processFile() {
+void readFile(char *file) {
 
 }
 
+void processInput() {
+  size_t inLen = 0;
+  char *input = NULL;
+  char *args = NULL;
+  static char *save;
+  bool running = true;
+  char req;
+  while(running) {
+    getline(&input, &inLen, stdin);
+    args = strtok_r(input, " ", &save); 
+    req = *args;
+    switch(req) {
+      case 'A' :
+
+      case 'F' : 
+      case 'S' : 
+      case 'R' : 
+      case 'C' : 
+      default : 
+    }
+  }
+}
+
+
 int main(int argc, char **argv) {
-  memset(mem, '.', sizeof(char) * MEMSIZE);
-  
+  head = (node *) malloc(sizeof(node));
+  head->name = '.';
+  head->size = MEMSIZE;
+  head->next = NULL;
+  processInput();
 }
